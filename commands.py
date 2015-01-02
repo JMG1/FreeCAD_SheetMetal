5bd662a8f102874f86ded239c4d064717ef19950
JMG1 <JMG1@users.noreply.github.com>
2015-01-02 12:05:37 +0100

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# FreeCAD Sheet Metal workbench
+# (c) 2015 Javier Martínez García
+
+#***************************************************************************
+#*   (c) Javier Martínez García 2015s                                       *   
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This macro is distributed in the hope that it will be useful,         *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        * 
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Lesser General Public License for more details.                   *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with FreeCAD; if not, write to the Free Software        * 
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************/
+
+
+import FreeCAD
+App = FreeCAD
+AAD = App.ActiveDocument
+
+def NewSMP():
+  # A dialog window to set the sheet metal project parameters will show up
+  # All this parameters can be changed later without dialog by checking this
+  # object features.
+  # User accesible features: Thickness, bend radius, K factor, relief slot dimension
+  # Operative features: base face, position, childs, bends, special geometries...
+  # NewSheetMetalProject (NSMP)
+  class NSMP:
+    def __init__(self, obj):
+      obj.addProperty("App::PropertyString",
+                      "ProjectName",
+                      "Project Data",
+                      "SheetMetal Project Name").ProjectName = "SM_Project"
+      
+      #User accesible features:
+      obj.addProperty("App::PropertyFloat",
+                      "SheetThickness",
+                      "Project Data",
+                      "Sheet thickness")
+      
+      obj.addProperty("App::PropertyFloat",
+                      "BendRadius",
+                      "Project Data",
+                      "Sheet bend radius")
+      
+      obj.addProperty("App::PropertyFloat",
+                      "KFactor",
+                      "Project Data",
+                      "bend K factor" )
+      
+      #Data stored for algorithms
+      obj.addProperty("App::PropertyMap",
+                      "DataDictionaryA",
+                      "Project Data"
+                      "Internal data" )
+      
+      obj.addProperty("App::PropertyMap",
+                      "DataDictionaryB",
+                      "Project Data"
+                      "Internal data" )
+      
+      obj.setEditorMode("DataDictionaryB", 2 )
+      obj.Proxy = self
+
+    def onChanged(self, fp, prop):
+      pass
+
+    def execute(self, fp):
+      pass
+
+  class ViewProviderNSMP:
+    def __init__(self, obj):
+      obj.Proxy = self
+    
+    def getIcon(self):
+      return """
+            /* XPM */
+            static char *dummy[]={
+            "32 32 2 1",
+            ". c None",
+            "# c #303030",
+            "................................",
+            "........#############...........",
+            "........#............#..........",
+            "........#....###......#.........",
+            "........#...#...#......#........",
+            "........#...#...#.......#.......",
+            "........#....###.........#......",
+            "........#.................#.....",
+            "........####################....",
+            "......###..................#....",
+            "....##..#..................#....",
+            "..##....#..................#....",
+            ".#...##.#..................#....",
+            ".#..#.#.#..................#....",
+            ".#.#..#.#..................#....",
+            ".#.#..#.#..................#....",
+            ".#.#..#.#..................#....",
+            ".#.#..#.#..................#....",
+            ".#.#..#.#..................#....",
+            ".#.####.#..................#....",
+            ".#......#..................#....",
+            ".###########################....",
+            ".............#............#.....",
+            ".............#.##########.#.....",
+            ".............#.#........#.#.....",
+            ".............#.#........#.#.....",
+            ".............#.#........#.#.....",
+            ".............#.#........#.#.....",
+            ".............#.##########.#.....",
+            ".............#............#.....",
+            ".............##############.....",
+            "................................"};
+            """
+    
+
+  a=AAD.addObject("App::DocumentObjectGroupPython","SM_Project")
+  NSMP(a)
+  ViewProviderNSMP(a.ViewObject)
+  AAD.recompute()
